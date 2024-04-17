4e910b58f88c8d36bfcfa03c4ead5926915598ea
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-04-17 16:58:11 -0700

    ViewChild and ngAfterViewInit

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Output, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, AfterViewInit, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
 import { User } from './auth-form.interface'
 import { FormsModule } from '@angular/forms'
 import { NgIf } from '@angular/common'
 
 import { AuthRememberComponent } from './auth-remember.component'
+import { AuthMessageComponent } from './auth-message.component'
 
 @Component({
   selector: 'auth-form',
   standalone: true,
-  imports: [FormsModule, NgIf, AuthRememberComponent],
+  imports: [FormsModule, NgIf, AuthRememberComponent, AuthMessageComponent],
   template: `
     <div class="auth-form">
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
@@ -22,26 +23,42 @@ import { AuthRememberComponent } from './auth-remember.component'
           <input type="password" name="password" ngModel />
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <div *ngIf="showMessage">
-          You will be logged in for 30 days
-        </div>
+        <auth-message
+          #message
+          [style.display]="showMessage ? 'inherit' : 'none'"
+        >
+        </auth-message>
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `,
 })
-export class AuthFormComponent implements AfterContentInit{
+export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   showMessage!: boolean;
 
   //Look up the component.
-  @ContentChildren(AuthRememberComponent) rememberList!: QueryList<AuthRememberComponent>
+  @ContentChildren(AuthRememberComponent) rememberList!: QueryList<AuthRememberComponent>;
+  
+  //Angular resolve the query statically, during the component initialization, rather than
+  //waiting until after the view has been created. It uses a template reference #message
+  @ViewChild('message', { static: true }) message!: AuthMessageComponent;
+
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  ngAfterViewInit(): void {
+    // console.log(this.message);
+  }
+
   ngAfterContentInit(): void {
-    if(this.rememberList){
-      this.rememberList.forEach((remember)=> remember.checked.subscribe((checked: boolean)=> {
-        this.showMessage = checked
-      }))
+    if (this.message) {
+      this.message.days = 30;
+    }
+    if (this.rememberList) {
+      this.rememberList.forEach((remember) =>
+        remember.checked.subscribe((checked: boolean) => {
+          this.showMessage = checked;
+        })
+      );
     }
   }
 
