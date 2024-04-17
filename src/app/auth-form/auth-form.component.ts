38d263aca1ff2aed82f11d5f22104cb5c5f06d29
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-04-17 16:06:35 -0700

    ContentChild and ngAfterContentInit

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,11 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ContentChild, AfterContentInit, contentChild } from '@angular/core';
 import { User } from './auth-form.interface'
 import { FormsModule } from '@angular/forms'
+import { NgIf } from '@angular/common'
+
+import { AuthRememberComponent } from './auth-remember.component'
 
 @Component({
   selector: 'auth-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, AuthRememberComponent],
   template: `
     <div class="auth-form">
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
@@ -19,13 +22,28 @@ import { FormsModule } from '@angular/forms'
           <input type="password" name="password" ngModel />
         </label>
         <ng-content select="auth-remember"></ng-content>
+        <div *ngIf="showMessage">
+          You will be logged in for 30 days
+        </div>
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `,
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements AfterContentInit{
+  showMessage!: boolean;
+
+  //Look up the component.
+  @ContentChild(AuthRememberComponent) remember!: AuthRememberComponent
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+  ngAfterContentInit(): void {
+    if(this.remember){
+      this.remember.checked.subscribe((checked:boolean)=> {
+        this.showMessage = checked
+      })
+    }
+  }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
