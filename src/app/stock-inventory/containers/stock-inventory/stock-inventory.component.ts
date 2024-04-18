a18a421481e849c37c3d6882a29e38c823da252b
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-04-18 16:34:54 -0700

    Added Product interface. Created form for Stock selector

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -5,22 +5,19 @@ import { FormControl, FormGroup, FormArray, ReactiveFormsModule } from '@angular
 import { StockBranchComponent } from "../../components/stock-branch/stock-branch.component";
 import { StockSelectorComponent } from "../../components/stock-selector/stock-selector.component";
 import { StockProductsComponent } from "../../components/stock-products/stock-products.component";
+import { Product } from '../../models/product.interface'
 
 @Component({
-    selector: 'stock-inventory',
-    standalone: true,
-    template: `
+  selector: 'stock-inventory',
+  standalone: true,
+  template: `
     <div class="stock-inventory">
       <form [formGroup]="form" (ngSubmit)="onSubmit()">
+        <stock-branch [parent]="form"> </stock-branch>
 
-        <stock-branch [parent]="form">
-        </stock-branch>
+        <stock-selector [parent]="form" [products]="products"> </stock-selector>
 
-        <stock-selector [parent]="form">
-        </stock-selector>
-
-        <stock-products [parent]="form">
-        </stock-products>
+        <stock-products [parent]="form"> </stock-products>
 
         <div class="stock-inventory__buttons">
           <button type="submit" [disabled]="form.invalid">Order stock</button>
@@ -30,10 +27,44 @@ import { StockProductsComponent } from "../../components/stock-products/stock-pr
       </form>
     </div>
   `,
-    styleUrls: ['./stock-inventory.component.scss'],
-    imports: [ReactiveFormsModule, JsonPipe, StockBranchComponent, StockSelectorComponent, StockProductsComponent]
+  styleUrls: ['./stock-inventory.component.scss'],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+    StockBranchComponent,
+    StockSelectorComponent,
+    StockProductsComponent,
+  ],
 })
 export class StockInventoryComponent {
+  products: Product[] = [
+    {
+      id: 1,
+      price: 100,
+      name: 'Macbook Pro',
+    },
+    {
+      id: 2,
+      price: 10,
+      name: 'USB-C-Adaptor',
+    },
+    {
+      id: 3,
+      price: 50,
+      name: 'IPod',
+    },
+    {
+      id: 4,
+      price: 60,
+      name: 'IPhone',
+    },
+    {
+      id: 5,
+      price: 50,
+      name: 'Apple Watch',
+    },
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(''),
@@ -41,10 +72,10 @@ export class StockInventoryComponent {
     }),
     selector: new FormGroup({
       product_id: new FormControl(''),
-      quantity: new FormControl(10)
+      quantity: new FormControl(10),
     }),
     // Create a collection of FormControls or FormGroups
-    stock: new FormArray([])
+    stock: new FormArray([]),
   });
 
   onSubmit() {
