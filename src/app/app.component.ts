1cac1c8ae3b69f0fac239671685cf6a60a0b87d0
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-04-18 14:52:34 -0700

    Setup Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
+import { StockInventoryComponent } from './stock-inventory/containers/stock-inventory/stock-inventory.component'
 
-import { User } from './auth-form/auth-form.interface';
-import { AuthFormComponent } from './auth-form/auth-form.component'
+
 
 @Component({
   selector: 'app-root',
-  imports: [AuthFormComponent],
   standalone: true,
+  imports: [StockInventoryComponent],
   template: `
     <div>
-      <auth-form 
-        (submitted)="createUser($event)">
-        <h3>Create account</h3>
-      </auth-form>
-      <auth-form 
-        (submitted)="loginUser($event)">
-        <h3>Login</h3>
-      </auth-form>
+      <stock-inventory></stock-inventory>
     </div>
   `,
 })
-export class AppComponent {
-  createUser(user: User) {
-    console.log('Create account', user);
-  }
-
-  loginUser(user: User) {
-    console.log('Login', user);
-  }
-}
+export class AppComponent {}
